a3ad5ad0370cc2f51560dd8d03fe159f01d736ee
Lilo Onwuzu <lpr422@gmail.com>
2016-04-29 13:45:48 -0700

    Failed spec to instantiate definition correctly

--- src/main/java/Definition.java
+++ src/main/java/Definition.java
@@ -1,7 +1,5 @@
 public class Definition {
 
-  public Definition(){
 
-  }
 
 }
---
+++ src/test/java/DefinitionTest.java
@@ -0,0 +1,11 @@
+import org.junit.*;
+import static org.junit.Assert.*;
+
+public class DefinitionTest{
+
+  @Test
+  public void Definition_instantiatesCorrectly_true(){
+    Definition newDefinition = new Definition();
+    assertEquals(false, newDefinition instanceof Definition);
+  }
+}
