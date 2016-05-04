14920a9c6e14ee3e4861f5a6ac5cf7be4ae3d623
Lilo Onwuzu <lpr422@gmail.com>
2016-05-04 01:07:56 -0700

    Word module and unit test passes

--- src/main/java/Word.java
+++ src/main/java/Word.java
@@ -1,51 +1,51 @@
-// import java.util.ArrayList;
-// import java.time.LocalDateTime;
-//
-// public class Word {
-//   // this is where we declare the attribute/property/instance variables of the Word Class. "private String mName" global scope instance variable is of reference type and so gets initialized as null. Global scope variables of primitive type gets initialized as '0' 'null' or 'false'. Local scope variables do not get automatically initialized and will cause compiler error if not initiatlized within.
-//   // private access instance variables so that other Word objects or all other objects in general (within this class package) do not have access to these variables within methods in which they are called
-//   // mName is necessary customize word objects
-//   private String mName;
-//   // id to keep track of number of words
-//   private int wordId;
-//   // arraylist to add the full list of word objects. There should exist only one list of words objects so it must be static/non-changing from word object to word object
-//   private static ArrayList<Word> wordInstances = new ArrayList<Word>();
-//   // i can apply a variable mDefinition of type Definition to any of Word's methods
-//   Definition mDefinition;
-//   // arraylist to add list of definitions per word. Should not be static so mDefinitions should vary from word object to another
-//   private ArrayList<Definition> mDefinitions;
-//
-//   // this is where we define the construct of the Word Class. If the constructor is not defined, the compiler definies one automatically assuming that no arguments are needed(Word())
-//   public Word(String wordName){
-//     // only the mName information is required to be defined before any word object can be made, every other function within the construct just happen whenever the Word(mName) construct is called
-//     mName = wordName;
-//     // automatically adds the newly formed word instance object to the full list of word objects
-//     wordInstances.add(this);
-//     // automatically creates an empty arraylist of definition objects called mDefinitions whenever a word object is created or instanced
-//     mDefinitions = new ArrayList<Definition>(mDefinition);
-//   }
-//
-//   // method to get word name. will be useful in the UI.
-//   public String getWordName(){
-//     return mName;
-//   }
-//
-//   // addDefinition(arg) defines a method to operate on a definition object. Definition mDefinition is an attribute/property/instance variable of the word class so all Word methods can operate on them
-//   // the argument of this method is needed so that the addDefinition method cannot be called upon on a Word object without including the name of the definition object that needs to be added
-//   // addDefinition(mdefinition) method adds the property mDefinition to the auto created arraylist variable mDefinitions
-//   // void methods do not return anything when they are called. it just updates or does something.
-//   public void ArrayList<Definition> addDefinition(Definition definition){
-//     mDefinitions.add(definition);
-//   }
-//
-//   // allWords() method returns the full list of word objects that have been instanced through the Word Class (only one list). will be useful for looping through all the word objects created since program execution time while in the UI
-//   public static ArrayList<Word> allWords(){
-//     return wordInstances;
-//   }
-//
-//   // returns the full arraylist of definition "objects" for this word class will be useful to loop through the list of definitions for a Word object in the UI
-//   public getmDefinitions(){
-//     return mDefinitions;
-//   }
-//
-// }
+import java.util.ArrayList;
+import java.time.LocalDateTime;
+
+public class Word {
+  // this is where we declare the attribute/property/instance variables of the Word Class. "private String mName" global scope instance variable is of reference type and so gets initialized as null. Global scope variables of primitive type gets initialized as '0' 'null' or 'false'. Local scope variables do not get automatically initialized and will cause compiler error if not initiatlized within.
+  // private access instance variables so that other Word objects or all other objects in general (within this class package) do not have access to these variables within methods in which they are called
+  // mName is necessary customize word objects
+  private String mName;
+  // id to keep track of number of words
+  private int wordId;
+  // arraylist to add the full list of word objects. There should exist only one list of words objects so it must be static/non-changing from word object to word object
+  private static ArrayList<Word> wordInstances = new ArrayList<Word>();
+  // i can apply a variable mDefinition of type Definition to any of Word's methods
+  Definition mDefinition;
+  // arraylist to add list of definitions per word. Should not be static so mDefinitions should vary from word object to another
+  private ArrayList<Definition> mDefinitions;
+
+  // this is where we define the construct of the Word Class. If the constructor is not defined, the compiler definies one automatically assuming that no arguments are needed(Word())
+  public Word(String wordName){
+    // only the mName information is required to be defined before any word object can be made, every other function within the construct just happen whenever the Word(mName) construct is called
+    mName = wordName;
+    // automatically adds the newly formed word instance object to the full list of word objects
+    wordInstances.add(this);
+    // automatically creates an empty arraylist of definition objects called mDefinitions whenever a word object is created or instanced
+    mDefinitions = new ArrayList<Definition>();
+  }
+
+  // method to get word name. will be useful in the UI and testing.
+  public String getWordName(){
+    return mName;
+  }
+
+  // addDefinition(arg) defines a method to operate on a definition object. Definition mDefinition is an attribute/property/instance variable of the word class so all Word methods can operate on them
+  // the argument of this method is needed so that the addDefinition method cannot be called upon on a Word object without including the name of the definition object that needs to be added
+  // addDefinition(mdefinition) method adds the property mDefinition to the auto created arraylist variable mDefinitions
+  // void methods do not return anything when they are called. it just updates or does something.
+  public void addDefinition(Definition definition){
+    mDefinitions.add(definition);
+  }
+
+  // allWords() method returns the full list of word objects that have been instanced through the Word Class (only one list). will be useful for looping through all the word objects created since program execution time while in the UI
+  public static ArrayList<Word> allWords(){
+    return wordInstances;
+  }
+
+  // returns the full arraylist of definition "objects" for this word class will be useful to loop through the list of definitions for a Word object in the UI
+  public ArrayList<Definition> getmDefinitions(){
+    return mDefinitions;
+  }
+
+}
--- src/test/java/WordTest.java
+++ src/test/java/WordTest.java
@@ -1,59 +1,59 @@
-// import org.junit.*;
-// import static org.junit.Assert.*;
-// import java.util.ArrayList;
-//
-// public class WordTest{
-//
-//   @Test
-//   // test to create instance/object of the word class
-//   public void Word_instantiatesCorrectly_true(){
-//     Word testWord = new Word("");
-//     assertEquals(true, testWord instanceof Word);
-//   }
-//
-//   @Test
-//   // test to create an object of the word class with the word name
-//   public void Word_instantiatesWithName_String(){
-//     Word testWord = new Word("word");
-//     assertEquals("word", testWord.getWordName());
-//   }
-//
-//   @Test
-//   // test to see that one word object is automatically added to the full arraylist of word objects
-//   public void Word_canAddOneWord_String(){
-//     Word testWord = new Word("");
-//     assertTrue(testWord.allWords().contains(testWord));
-//   }
-//
-//   @Test
-//   // test to see that allWords() method adds multiple word objects automatically to the full arraylist of word objects
-//   public void allWords_canAddMultipleWords_String(){
-//     Word testWord = new Word("");
-//     Word testWord2 = new Word("");
-//     assertTrue(testWord.allWords().contains(testWord));
-//     assertTrue(testWord.allWords().contains(testWord2));
-//   }
-//
-//   @Test
-//   // test to see that addDefinition(arg) method adds one definition to the arraylist of definitions for one word object/instance
-//   public void addDefinition_canAddOneDefinitionForOneWord_String(){
-//     Word testWord = new Word("word");
-//     Definition testDefinition = new Definition("this word is good");
-//     testWord.addDefinition(testDefinition);
-//     assertTrue(testWord.addDefinition(testDefinition).contains(testDefinition));
-//   }
-//
-//   @Test
-//   // test to see that addDefinition(arg) method adds multiple definitions to the arraylist of definitions for one word object/instance
-//   public void addDefinition_canAddMultipleDefinitionsForOneWord_String(){
-//     Word testWord = new Word("word");
-//     Definition testDefinition = new Definition("this word is good");
-//     Definition testDefinition2 = new Definition("this word is really good");
-//     testWord.addDefinition(testDefinition);
-//     testWord.addDefinition(testDefinition2);
-//     assertEquals("this word is good", testWord.addDefinition(testDefinition).get(0).getDefinition());
-//     assertTrue(testWord.addDefinition(testDefinition2).contains(testDefinition2));
-//   }
-//
-//
-// }
+import org.junit.*;
+import static org.junit.Assert.*;
+import java.util.ArrayList;
+
+public class WordTest{
+
+  @Test
+  // test to create instance/object of the word class
+  public void Word_instantiatesCorrectly_true(){
+    Word testWord = new Word("");
+    assertEquals(true, testWord instanceof Word);
+  }
+
+  @Test
+  // test to see if an object of the word class returns its name correctly
+  public void Word_instantiatesWithName_String(){
+    Word testWord = new Word("word");
+    assertEquals("word", testWord.getWordName());
+  }
+
+  @Test
+  // test to see that one word object is automatically added to the full arraylist of word objects. allWords() is a Word class method that returns the most updated static list of word objects since program execution
+  public void Word_canAddOneWord_String(){
+    Word testWord = new Word("word");
+    // assertTrue is a method of the junit class library that passes this test if the argument evaluate to true
+    assertTrue(testWord.allWords().contains(testWord));
+  }
+
+  @Test
+  // test to see that allWords() method adds multiple word objects automatically to the full arraylist of word objects. allWords() can be applied to any word object but will return the most updated list of wordInstances
+  public void allWords_canAddMultipleWords_String(){
+    Word testWord = new Word("my word");
+    Word testWord2 = new Word("your word");
+    assertTrue(testWord.allWords().contains(testWord));
+    assertTrue(testWord.allWords().contains(testWord2));
+  }
+
+  @Test
+  // test to see that addDefinition(arg) method adds one definition to the arraylist of definitions for one word object/instance
+  public void addDefinition_canAddOneDefinitionForOneWord_String(){
+    Word testWord = new Word("word");
+    Definition testDefinition = new Definition("this word is good");
+    testWord.addDefinition(testDefinition);
+    assertTrue(testWord.getmDefinitions().contains(testDefinition));
+  }
+
+  @Test
+  // test to see that addDefinition(arg) method adds multiple definitions to the arraylist of definitions for one word object/instance
+  public void addDefinition_canAddMultipleDefinitionsForOneWord_String(){
+    Word testWord = new Word("word");
+    Definition testDefinition = new Definition("this word is good");
+    Definition testDefinition2 = new Definition("this word is really good");
+    testWord.addDefinition(testDefinition);
+    testWord.addDefinition(testDefinition2);
+    assertTrue(testWord.getmDefinitions().contains(testDefinition));
+    assertEquals("this word is really good", testWord.getmDefinitions().get(1).getDefinition());
+  }
+
+}
